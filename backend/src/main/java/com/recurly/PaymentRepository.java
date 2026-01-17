9ad9f084a82bf62bf6add582cfe2e109cbe313ca
meharparnami14 <meharparnami14@gmail.com>
2026-01-17 23:42:30 +0530

    Update PaymentRepository.java

--- backend/src/main/java/com/recurly/PaymentRepository.java
+++ backend/src/main/java/com/recurly/PaymentRepository.java
@@ -1,9 +1,6 @@
-package com.recurly.repository;
+package com.recurly;
 
-import com.recurly.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
